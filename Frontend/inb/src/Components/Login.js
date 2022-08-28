5f7558ee942a9d46703c8e14cefeb05ebab85eac
Dhruv SIngh <dhruv1647>
2022-08-28 11:50:37 +0530

    Create user and user login working Sucess modified

---
+++ Frontend/inb/src/Components/Login.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react'
+
+function Login() {
+	const [mystate, changestate] = useState("");
+	const loginrequest = async (Email, password) => {
+		const url = 'http://localhost:3012/api/auth/Login';
+		const response = await fetch(url, {
+			method: 'post',
+			mode: 'cors',
+			headers: {
+				'content-Type': 'application/json'
+			},
+			body: JSON.stringify({ email: Email, password: password })
+		})
+		console.log(response);
+	}
+	const onchange1 = (e) => {
+		console.log(mystate);
+		changestate({ ...mystate, [e.target.name]: [e.target.value] })
+	}
+	return (
+		<div className='m-7'>
+			<p className='mb-6 text-5xl font-bold'>Login</p>
+			<div class="mb-3 row">
+				<label for="staticEmail" class="col-sm-2 col-form-label">Email</label>
+				<div class="col-sm-10">
+					<input name="email" value={mystate.email} onChange={onchange1} type="text" readonly class="form-control-plaintext" id="staticEmail" />
+				</div>
+			</div>
+			<div class="mb-3 row">
+				<label for="inputPassword" class="col-sm-2 col-form-label">Password</label>
+				<div class="col-sm-10">
+					<input name="password" value={mystate.password} onChange={onchange1} type="password" class="form-control" id="inputPassword" />
+				</div>
+			</div>
+			<div>
+				<button className="btn btn-outline-success float-right mx-11" type="submit" onClick={() => {
+					if (typeof (mystate.name) == "string") { loginrequest(mystate.email, mystate.password) }
+					else {
+						loginrequest(mystate.email[0], mystate.password[0])
+					}
+				}
+				}>Login</button>
+			</div>
+		</div>
+	)
+}
+
+export default Login
---
+++ Frontend/inb/src/Components/SignUp.js
@@ -0,0 +1,53 @@
+import React, { useState } from 'react'
+
+function SignUp() {
+	const [credentials, setcredentials] = useState(""); 
+	const signuprequest = async (name, Email, password) => {  
+		const url = 'http://localhost:3012/api/auth/Create_User';
+		const response = await fetch(url, {
+			method: 'post',
+			mode: 'cors',
+			headers: {
+				'content-Type': 'application/json'
+			},
+			body: JSON.stringify({ name: name, email: Email, password: password })})
+			console.log(response);
+		}
+
+		const onchangehandler1 = (e) => {
+			setcredentials({ ...credentials, [e.target.name]: [e.target.value] })
+		}
+		return (
+			<div>
+				<p className='ml-5 text-5xl font-bold'>Sign Up</p>
+				<div className='m-7'>
+					<div class="mb-3 row">
+						<label for="name" class="col-sm-2 col-form-label">Name</label>
+						<div class="col-sm-10">
+							<input type="emaiil" name="name" class="form-control-plaintext" value={credentials.name} onChange={onchangehandler1} placeholder='Your Name' />
+						</div>
+					</div>
+					<div class="mb-3 row">
+						<label for="staticEmail" class="col-sm-2 col-form-label">Email</label>
+						<div class="col-sm-10">
+							<input type="text" readonly class="form-control-plaintext" name="Email" id="staticEmail" onChange={onchangehandler1} value={credentials.Email} placeholder="email@example.com" />
+						</div>
+					</div>
+					<div class="mb-3 row">
+						<label for="inputPassword" class="col-sm-2 col-form-label">Password</label>
+						<div class="col-sm-10">
+							<input type="password" name="password" onChange={onchangehandler1} value={credentials.password} placeholder='Your Passwoord' class="form-control" id="inputPassword" />
+						</div>
+					</div>
+				</div>
+				<button className="btn btn-outline-success float-right mx-11" type="submit" onClick={() => {
+					if (typeof (credentials.name) == "string") { signuprequest(credentials.name, credentials.email, credentials.password) }
+					else {
+						signuprequest(credentials.name[0], credentials.Email[0], credentials.password[0])
+					}
+				}
+				}>Login</button>
+			</div>)
+	}
+
+	export default SignUp
